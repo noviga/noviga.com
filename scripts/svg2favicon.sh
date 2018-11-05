84218a5cbbb40643f18dd96edd2cca74a9f8c4d2
Shamil Yakupov <s.yakupov@noviga.com>
2018-11-05 23:56:03 +0300

    Rework icons

--- scripts/svg2favicon.sh
+++ scripts/svg2favicon.sh
@@ -7,15 +7,19 @@ cd "$(dirname "$(readlink -f "${0}")")"
 
 # SVG to PNG
 PNGLIST=''
-for SIZE in 16 32 48
+for SIZE in 16 32
 do
-  inkscape -z ../img/noviga-icon.svg --export-png=favicon${SIZE}.png --export-width=$SIZE
-  PNGLIST="$PNGLIST favicon${SIZE}.png"
+  inkscape -z ../img/noviga-favicon.svg --export-png=../docs/img/favicon-${SIZE}.png --export-width=$SIZE
+  PNGLIST="$PNGLIST ../docs/img/favicon-${SIZE}.png"
 done
 
 
 # PNG to ICO
-icotool -o ../docs/favicon.ico -c $PNGLIST
+icotool -o ../docs/img/favicon.ico -c $PNGLIST
 
-# Clean up
-rm -rf $PNGLIST
+# Various sized PNG
+for SIZE in 48 57 72 114 120 144 152 256
+do
+  inkscape -z ../img/noviga-favicon.svg --export-png=../docs/img/favicon-${SIZE}.png --export-width=$SIZE
+done
+
